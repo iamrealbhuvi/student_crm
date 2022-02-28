078f4830931e32fcbfea11fab4c96d31300d5c36
= <=>
2022-02-28 12:23:21 +0530

    addition of data in file system

--- Dashboard/institution/students/index.php
+++ Dashboard/institution/students/index.php
@@ -63,7 +63,7 @@ $instit_meta_info_array = mysqli_fetch_assoc($instit_meta_info_result);
         <hr class="horizontal light mt-0 mb-2">
         <div class="collapse navbar-collapse  w-auto  max-height-vh-100" id="sidenav-collapse-main">
             <ul class="navbar-nav">
-            <li class="nav-item">
+                <li class="nav-item">
                     <a class="nav-link text-dark " href="../index.php">
                         <div class="text-dark text-center me-2 d-flex align-items-center justify-content-center">
                             <i class="material-icons opacity-10">dashboard</i>
@@ -95,7 +95,7 @@ $instit_meta_info_array = mysqli_fetch_assoc($instit_meta_info_result);
                         <span class="nav-link-text ms-1">Classrooms</span>
                     </a>
                 </li>
-                
+
                 <li class="nav-item">
                     <a class="nav-link text-white active bg-gradient-secondary" href="./index.php">
                         <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
@@ -136,120 +136,127 @@ $instit_meta_info_array = mysqli_fetch_assoc($instit_meta_info_result);
 
 
 
-
-
-
-
-
-
-
-    <!-- Toggle Menu for smaller Devices -->
-
-    <div class="fixed-plugin " id="sidebar-toggler">
-        <a class="fixed-plugin-button text-dark position-fixed px-3 py-2" onclick="document.getElementById('sidebar-toggler').classList.add('show');">
-            <i class="material-icons py-2">widgets</i>
-        </a>
-        <div class="card shadow-lg">
-            <div class="card-header pb-0 pt-3">
-                <div class="float-start">
-                    <h5 class="mt-3 mb-0"><?php echo $instit_fetched_array['schoolname'] ?></h5>
-                    <p></p>
-                </div>
-                <div class="float-end mt-4">
-                    <button class="btn btn-link text-dark p-0 fixed-plugin-close-button" onclick="document.getElementById('sidebar-toggler').classList.remove('show');">
-                        <i class="material-icons">clear</i>
-                    </button>
-                </div>
-                <!-- End Toggle Button -->
-                <div class="sidenav-header">
-                    <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-                    <a class="navbar-brand m-0 w-75 " href="./instit_profile/index.php" target="_blank">
-                        <img src="../../../photostore/institution-profiles/<?php echo $instit_fetched_array['schoolusername']; ?>/<?php echo $instit_fetched_array['schoolprofile'] ?>" class="navbar-brand-img h-100" alt="main_logo">
-                        <span class="ms-1 font-weight-bold fs-6 text-dark text-break"><?php echo $instit_fetched_array['schoolusername'] ?></span>
-                    </a>
-                </div>
-            </div>
-            <div class="card-body pb-0 pt-2">
-
-                <hr class="horizontal light mt-0 mb-2">
-                <div class=" w-auto  max-height-vh-100" id="sidenav-collapse-main">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link text-dark d-flex rounded-3 " href="../index.php" >
-                                <div class="text-dark text-center me-2 d-flex align-items-center justify-content-center">
-                                    <i class="material-icons opacity-10 ms-3">dashboard</i>
-                                </div>
-                                <span class="nav-link-text ms-1">Dashboard</span>
-                            </a>
-                        </li>
-
-                        
-                        <li class="nav-item">
-                            <a class="nav-link text-dark d-flex rounded-3" href="../pages/tables.html">
-                                <div class="text-dark text-center me-2 d-flex align-items-center justify-content-center">
-                                    <i class="material-icons opacity-10 ms-3">table_view</i>
-                                </div>
-                                <span class="nav-link-text ms-1">Exams</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-dark d-flex rounded-3" href="../pages/billing.html">
-                                <div class="text-dark text-center me-2 d-flex align-items-center justify-content-center">
-                                    <i class="material-icons opacity-10 ms-3">calendar_today</i>
-                                </div>
-                                <span class="nav-link-text ms-1">Schedule</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-dark d-flex rounded-3 " href="../pages/virtual-reality.html">
-                                <div class="text-dark text-center me-2 d-flex align-items-center justify-content-center">
-                                    <i class="material-icons opacity-10 ms-3">class</i>
-                                </div>
-                                <span class="nav-link-text ms-1">Classrooms</span>
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link text-white active bg-gradient-secondary d-flex rounded-3" href="../students/index.php">
-                                <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                                    <i class="material-icons opacity-10 ms-3">people</i>
-                                </div>
-                                <span class="nav-link-text ms-1">Students</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-dark d-flex rounded-3" href="../pages/rtl.html">
-                                <div class="text-dark text-center me-2 d-flex align-items-center justify-content-center">
-                                    <i class="material-icons opacity-10 ms-3">emoji_events</i>
-                                </div>
-                                <span class="nav-link-text ms-1">Events</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-dark d-flex rounded-3" href="../pages/notifications.html">
-                                <div class="text-dark text-center me-2 d-flex align-items-center justify-content-center">
-                                    <i class="material-icons opacity-10 ms-3">currency_rupee</i>
-                                </div>
-                                <span class="nav-link-text ms-1">Fee Center</span>
-                            </a>
-                        </li>
-
-                    </ul>
-                </div>
-                <div class="sidenav-footer position-absolute w-100 bottom-0 ">
-                    <div class="mx-auto">
-                        <a class="btn bg-gradient-secondary mt-4 w-75" href="#to_profile" type="button">Profile</a>
-                    </div>
-                    <div class="mx-auto">
-                        <a class="btn bg-gradient-secondary mt-2 w-75" href="to Signout" type="button">Sign Out</a>
-                    </div>
-                </div>
-
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+        <div class="row mb-4 mt-4 mx-auto my-3 ">
+            <div class="col-lg-11 col-md-11 mb-md-0 mb-4 text-center fs-3 text-dark">
+                <div><?php echo $instit_fetched_array['schoolname'] ?> - Students</div>
             </div>
         </div>
-    </div>
+
+        
 
 
+
+
+
+
+        <!-- Toggle Menu for smaller Devices -->
+
+        <div class="fixed-plugin " id="sidebar-toggler">
+            <a class="fixed-plugin-button text-dark position-fixed px-3 py-2" onclick="document.getElementById('sidebar-toggler').classList.add('show');">
+                <i class="material-icons py-2">widgets</i>
+            </a>
+            <div class="card shadow-lg">
+                <div class="card-header pb-0 pt-3">
+                    <div class="float-start">
+                        <h5 class="mt-3 mb-0"><?php echo $instit_fetched_array['schoolname'] ?></h5>
+                        <p></p>
+                    </div>
+                    <div class="float-end mt-4">
+                        <button class="btn btn-link text-dark p-0 fixed-plugin-close-button" onclick="document.getElementById('sidebar-toggler').classList.remove('show');">
+                            <i class="material-icons">clear</i>
+                        </button>
+                    </div>
+                    <!-- End Toggle Button -->
+                    <div class="sidenav-header">
+                        <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+                        <a class="navbar-brand m-0 w-75 " href="./instit_profile/index.php" target="_blank">
+                            <img src="../../../photostore/institution-profiles/<?php echo $instit_fetched_array['schoolusername']; ?>/<?php echo $instit_fetched_array['schoolprofile'] ?>" class="navbar-brand-img h-100" alt="main_logo">
+                            <span class="ms-1 font-weight-bold fs-6 text-dark text-break"><?php echo $instit_fetched_array['schoolusername'] ?></span>
+                        </a>
+                    </div>
+                </div>
+                <div class="card-body pb-0 pt-2">
+
+                    <hr class="horizontal light mt-0 mb-2">
+                    <div class=" w-auto  max-height-vh-100" id="sidenav-collapse-main">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link text-dark d-flex rounded-3 " href="../index.php">
+                                    <div class="text-dark text-center me-2 d-flex align-items-center justify-content-center">
+                                        <i class="material-icons opacity-10 ms-3">dashboard</i>
+                                    </div>
+                                    <span class="nav-link-text ms-1">Dashboard</span>
+                                </a>
+                            </li>
+
+
+                            <li class="nav-item">
+                                <a class="nav-link text-dark d-flex rounded-3" href="../pages/tables.html">
+                                    <div class="text-dark text-center me-2 d-flex align-items-center justify-content-center">
+                                        <i class="material-icons opacity-10 ms-3">table_view</i>
+                                    </div>
+                                    <span class="nav-link-text ms-1">Exams</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark d-flex rounded-3" href="../pages/billing.html">
+                                    <div class="text-dark text-center me-2 d-flex align-items-center justify-content-center">
+                                        <i class="material-icons opacity-10 ms-3">calendar_today</i>
+                                    </div>
+                                    <span class="nav-link-text ms-1">Schedule</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark d-flex rounded-3 " href="../pages/virtual-reality.html">
+                                    <div class="text-dark text-center me-2 d-flex align-items-center justify-content-center">
+                                        <i class="material-icons opacity-10 ms-3">class</i>
+                                    </div>
+                                    <span class="nav-link-text ms-1">Classrooms</span>
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link text-white active bg-gradient-secondary d-flex rounded-3" href="../students/index.php">
+                                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                                        <i class="material-icons opacity-10 ms-3">people</i>
+                                    </div>
+                                    <span class="nav-link-text ms-1">Students</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark d-flex rounded-3" href="../pages/rtl.html">
+                                    <div class="text-dark text-center me-2 d-flex align-items-center justify-content-center">
+                                        <i class="material-icons opacity-10 ms-3">emoji_events</i>
+                                    </div>
+                                    <span class="nav-link-text ms-1">Events</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark d-flex rounded-3" href="../pages/notifications.html">
+                                    <div class="text-dark text-center me-2 d-flex align-items-center justify-content-center">
+                                        <i class="material-icons opacity-10 ms-3">currency_rupee</i>
+                                    </div>
+                                    <span class="nav-link-text ms-1">Fee Center</span>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                    <div class="sidenav-footer position-absolute w-100 bottom-0 ">
+                        <div class="mx-auto">
+                            <a class="btn bg-gradient-secondary mt-4 w-75" href="#to_profile" type="button">Profile</a>
+                        </div>
+                        <div class="mx-auto">
+                            <a class="btn bg-gradient-secondary mt-2 w-75" href="to Signout" type="button">Sign Out</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </main>
 
 
 
